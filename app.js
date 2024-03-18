661def0d4e54f5d74647b9ad41991ba541dbe118
Petra <petra@hajo.home>
2024-03-18 17:20:10 +0100

    added querySelectors

---
+++ app.js
@@ -0,0 +1,8 @@
+import jsonData from './data.json';
+console.log("jsonData",jsonData)
+
+const buttonAll = document.querySelector(".button _primary");
+const rightArrow = document.querySelector(".right-arrow");
+const toggle = document.querySelector(".toggle");
+
+ 
