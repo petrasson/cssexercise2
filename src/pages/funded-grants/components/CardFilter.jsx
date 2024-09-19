2be1a6b8c9fd1ab3909c251861ccbb8d936b26d3
Petra Eriksson <eriksson.petra@gmail.com>
2024-09-19 14:28:02 +0200

    solved the filterControl and showing the gradiant in mobile version

--- src/pages/funded-grants/components/CardFilter.jsx
+++ src/pages/funded-grants/components/CardFilter.jsx
@@ -11,94 +11,57 @@ const CardFilterWrapper = styled.div`
   flex-direction: column;
   align-items: start;
 
-.filter-container {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  position: relative;
-  width: 100%;
-  overflow: hidden;
-  z-index: 3;
-  margin-bottom: 20px;
-}
-
-.filter-container::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 20%;
-  height: 100%;
-  background-image: linear-gradient(
-    to right,
-    rgb(35 35 52 / 0%),
-    rgb(35 35 52 / 100%)
-  );
-  z-index: 5;
-}
-
-.filter-container::after {
-  content: "";
-  position: absolute;
-  top: 15px;
-  right: 2px;
-  background-image: url("images/rightarrow.svg");
-  width: 20px;
-  height: 20px;
-  z-index: 6;
-  background-repeat: no-repeat;
-}
 
 
 
 /****** TOGGLE ******/
 
-.toggle-wrapper {
-  display: flex;
-  flex-direction: row;
-  max-width: 1440px;
-  color: var(--secondary-text-color);
-  align-items: center;
-  cursor: pointer;
-}
+// .toggle-wrapper {
+//   display: flex;
+//   flex-direction: row;
+//   max-width: 1440px;
+//   color: var(--secondary-text-color);
+//   align-items: center;
+//   cursor: pointer;
+// }
 
-#toggle {
-  appearance: none;
-  position: relative;
-  width: 50px;
-  height: 20px;
-  border-radius: 25px;
-  background-color: var(--accent-color);
-  transition: background 0.5s;
-  cursor: pointer;
-}
+// #toggle {
+//   appearance: none;
+//   position: relative;
+//   width: 50px;
+//   height: 20px;
+//   border-radius: 25px;
+//   background-color: var(--accent-color);
+//   transition: background 0.5s;
+//   cursor: pointer;
+// }
 
-#toggle::after {
-  content: "";
-  position: absolute;
-  top: 50%;
-  left: 22px;
-  transform: translate(0, -50%);
-  border-radius: 50%;
-  width: 30px;
-  height: 30px;
-  background-color: var(--primary-text-color);
-  transition: left 0.3s;
-}
+// #toggle::after {
+//   content: "";
+//   position: absolute;
+//   top: 50%;
+//   left: 22px;
+//   transform: translate(0, -50%);
+//   border-radius: 50%;
+//   width: 30px;
+//   height: 30px;
+//   background-color: var(--primary-text-color);
+//   transition: left 0.3s;
+// }
 
-#toggle:checked {
-  background-color: var(--primary-text-color);
-}
+// #toggle:checked {
+//   background-color: var(--primary-text-color);
+// }
 
-#toggle:checked::after {
-  left: -3px;
-  background-color: var(--primary-text-color);
-}
+// #toggle:checked::after {
+//   left: -3px;
+//   background-color: var(--primary-text-color);
+// }
 
-.toggle-wrapper .toggle-text {
-  padding-left: 27px;
-  font-size: 16px;
-}
+// .toggle-wrapper .toggle-text {
+//   padding-left: 27px;
+//   font-size: 16px;
+// }
 
 
 /*********************************** WEB VERSION *********************************/
@@ -106,13 +69,13 @@ const CardFilterWrapper = styled.div`
 @media only screen and (width >= 1305px) {
 
 
-.filter-container::after {
-  display: none;
-}
+// .filter-container::after {
+//   display: none;
+// }
 
-.filter-container::before {
-  display: none;
-}
+// .filter-container::before {
+//   display: none;
+// }
 
 
 `;
@@ -139,9 +102,7 @@ function CardFilter() {
 
   return (
     <CardFilterWrapper>
-      <div className='filter-container'>
-        <FilterControl handleFilter={handleFilter} widthToggle={true} />
-      </div>
+      <FilterControl handleFilter={handleFilter} withToggle={true} />
 
       <CardHolder cards={filteredData} filterType={filteredData} />
     </CardFilterWrapper>
--- src/shared_components/FilterControl.jsx
+++ src/shared_components/FilterControl.jsx
@@ -5,104 +5,151 @@ import Toggle from "../pages/funded-grants/components/Toggle";
 
 const FilterControlWrapper = styled.div`
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: start;
-  gap: 60px;
-  margin: 0;
-  padding: 0;
-  overflow-x: auto;
-  color: var(--secondary-text-color);
-  position: relative;
-  scrollbar-width: none;
+  flex-direction: column;
   width: 100%;
-  display: flex;
-  scrollbar-width: none;
-
-  :first-child {
-    min-width: 50px;
-  }
 
   li {
     list-style-type: none;
   }
 
-  // .menu-filter {
-  //   min-width: 150px;
-  //   position: relative;
-  //   z-index: 4;
-  //   border: none;
-  //   color: var(--primary-text-color);
-  //   font-size: 15px;
-  //   cursor: pointer;
-  //   background-color: transparent;
+  .filter-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+    overflow-x: auto;
+    z-index: 3;
+    margin-bottom: 20px;
 
-    &._discover {
-      min-width: 100px;
+    .filter-wrapper {
+      gap: 10px;
     }
   }
 
-  @media only screen and (width >= 1305px) {
+  .filter-container::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 20%;
+    height: 100%;
+    background-image: linear-gradient(
+      to right,
+      rgb(35 35 52 / 0%),
+      rgb(35 35 52 / 100%)
+    );
+    z-index: 5;
+    pointer-events: none;
+  }
+
+  .filter-container::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    right: 2px;
+    background-image: url("../../images/rightarrow.svg");
+    transform: translateY(-50%);
+    width: 20px;
+    height: 20px;
+    z-index: 9;
+    background-repeat: no-repeat;
+    pointer-events: none;
+  }
+
+  .filter-wrapper {
     display: flex;
-    padding: 0;
-    overflow-x: hidden;
-    width: auto;
-    gap: 15px;
-    font-size: 16px;
-    line-height: 24px;
+    flex-direction: row;
+    align-items: start;
+    gap: 10px;
+    margin: 0px;
+    overflow-x: auto;
+    scrollbar-width: none;
+    color: var(--secondary-text-color);
+    position: relative;
+    z-index: 4;
+  }
+
+  @media only screen and (width >= 1305px) {
+    .filter-container {
+      overflow: visible;
+
+      .filter-wrapper {
+        gap: 60px;
+      }
+    }
+
+    .filter-container::before,
+    .filter-container::after {
+      display: none;
+    }
+
+    .filter-wrapper {
+      overflow-x: hidden;
+      gap: 60px;
+    }
   }
 `;
 
 function FilterControl({ handleFilter, withToggle }) {
   return (
     <FilterControlWrapper>
-      <Button
-        text='All'
-        type='primary'
-        filterType='All'
-        onClick={() => handleFilter("All")}
-      />
-      <li>
+      <div className='filter-container'>
         <Button
-          text='Technical/Tool Development'
-          type='text'
-          filterType='Technical/Tool Development'
-          onClick={() => handleFilter("Technical/Tool Development")}
+          text='All'
+          type='primary'
+          filterType='All'
+          onClick={() => handleFilter("All")}
         />
-      </li>
-      <li>
-        <Button
-          text='Governance'
-          type='text'
-          filterType='Governance'
-          onClick={() => handleFilter("Governance")}
-        />
-      </li>
-      <li>
-        <Button
-          text='Growth / Marketing'
-          type='text'
-          filterType='Growth / Marketing'
-          onClick={() => handleFilter("Growth / Marketing")}
-        />
-      </li>
-      <li>
-        <Button
-          text='Analytics'
-          type='text'
-          filterType='Analytics'
-          onClick={() => handleFilter("Analytics")}
-        />
-      </li>
-      <li>
-        <Button
-          text='Third Party Provider'
-          type='text'
-          filterType='Third Party Provider'
-          onClick={() => handleFilter("Third Party Provider")}
-        />
-      </li>
-      ${withToggle && <Toggle />}
+        <div className='filter-wrapper'>
+          <li>
+            <Button
+              text='Technical/Tool Development'
+              type='text'
+              filterType='Technical/Tool Development'
+              onClick={() => handleFilter("Technical/Tool Development")}
+            />
+          </li>
+          <li>
+            <Button
+              text='Governance'
+              type='text'
+              filterType='Governance'
+              onClick={() => handleFilter("Governance")}
+            />
+          </li>
+          <li>
+            <Button
+              text='Growth / Marketing'
+              type='text'
+              filterType='Growth / Marketing'
+              onClick={() => handleFilter("Growth / Marketing")}
+            />
+          </li>
+          <li>
+            <Button
+              text='Analytics'
+              type='text'
+              filterType='Analytics'
+              onClick={() => handleFilter("Analytics")}
+            />
+          </li>
+          <li>
+            <Button
+              text='Third Party Provider'
+              type='text'
+              filterType='Third Party Provider'
+              onClick={() => handleFilter("Third Party Provider")}
+            />
+          </li>
+        </div>
+      </div>
+      {withToggle && (
+        <div className='toggle-container'>
+          <Toggle />
+        </div>
+      )}
     </FilterControlWrapper>
   );
 }
@@ -113,32 +160,3 @@ FilterControl.propTypes = {
 };
 
 export default FilterControl;
-
-// <li>
-//         <button
-//           className='menu-filter'
-//           data-filter='Technical/Tool Development'
-//         >
-//           Technical/Tool Development
-//         </button>
-//       </li>
-//       <li>
-//         <button className='menu-filter' data-filter='Governance'>
-//           Governance
-//         </button>
-//       </li>
-//       <li>
-//         <button className='menu-filter' data-filter='Growth / Marketing'>
-//           Growth/Marketing
-//         </button>
-//       </li>
-//       <li>
-//         <button className='menu-filter' data-filter='Analytics'>
-//           Analytics
-//         </button>
-//       </li>
-//       <li>
-//         <button className='menu-filter' data-filter='Third Party Provider'>
-//           Third party provider
-//         </button>
-//       </li>
